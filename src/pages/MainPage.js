98de3b9a1512f6bac6e248cc6fb86f56ddcfb873
Jaa125 <75535272+Jaa125@users.noreply.github.com>
2023-02-26 14:49:04 +0100

    fixing responsive stuff

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,18 @@
 function MainPage() {
   return (
     <div className="puzzle-page">
-      <h3>How to play?</h3>
+      <h3>How to play ?</h3>
       <p>
-        Latius iam disseminata licentia onerosus bonis omnibus Caesar nullum
-        post haec adhibens modum orientis latera cuncta vexabat nec honoratis
-        parcens nec urbium primatibus nec plebeiis.
+      
+        Once You successfully logged in,
+         You will see in a middle screen a puzzle.
+          To start playing, You must click on it,
+           which will redirect You to an Instagram account page. 
+           In order to become eligible for the grand prize,
+            You must follow the Instagram page You have been directed to.
+             Once You have completed this step, You will be pre-selected to win one of the big prizes on offer.
+              It is important to note that only players who follow the Instagram account will be eligible to win, 
+              so it is essential that You complete this step in order to increase Your chances of success. Good luck and have fun!
       </p>
     </div>
   );
